7e828531059393557f474fdbc081ab60cb55201e
ealfr <mainajanelogan@gmail.com>
2023-03-06 05:34:56 -0800

    C - Functions, nested loops tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,17 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
-
 	while (i < 10)
 	{
 		ch = 'a';
 		while (ch <= 'z')
 		{
 			_putchar(ch);
- 	        	ch++;
+			ch++;
 		}
-			_putchar('\n');
-			i++;
+		_putchar('\n');
+		i++;
 	}
 }
+
 
